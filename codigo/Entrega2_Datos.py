d4c5ad17b55e8cb9f2e1bbdec3ad0e88fc9794af
MauricioCa07 <104088460+MauricioCa07@users.noreply.github.com>
2022-10-17 08:57:14 -0500

    Update Entrega2_Datos.py

--- codigo/Entrega2_Datos.py
+++ codigo/Entrega2_Datos.py
@@ -1,88 +1,69 @@
 import pandas as pd
 import gmplot 
+from Adicional import adicional
 
 df=pd.read_csv('C:\Programacion_Visual\Datos.py\calles_de_medellin_con_acoso.csv',sep=';')
 df_Filtrado = df.fillna({"harassmentRisk":df['harassmentRisk'].mean()})
 
+
+adicional.bienvenida()
 grafo={}
+grafo=adicional.crear_Grafo(df_Filtrado,grafo)
 
-#con esto creo el grafo, es un una tupla dentro de un diccionario anidado
-for linea in df_Filtrado.index:
-    #Esta es una tupla que contiene el promedio de riego y longitd, así como sus valores por aparte será util pa calcular 3 rutas
-    temp=((df_Filtrado["harassmentRisk"][linea]+df_Filtrado["length"][linea])/2,df_Filtrado["length"][linea],df_Filtrado["harassmentRisk"])
-    if df_Filtrado["origin"][linea] not in grafo:
-        grafo[df_Filtrado["origin"][linea]]={df_Filtrado["destination"][linea]:temp}
+print("Digite la coordinada inicial, sin los parentesis:")
+nodo_Inicial="("+str(input())+")"
+print("Digite la coordinada final, sin los parentesis:")
+nodo_FInal="("+str(input())+")"
+temp=""
+continuar="si"
+
+while continuar=="si":    
+    adicional.información()
+    n=int(input())
+    if n==1:
+        grafo_Temp={}
+        #creo un grafo aparte del cuya en la que la tupla se cambia por el promedio de riesgo y longitud
+        #lo hize así pa cuando toque calcular los 3 caminos distintos 
+        for clave in grafo:
+            grafo_Temp[clave]=grafo[clave]
+            for valor in grafo[clave]:
+                grafo_Temp[clave][valor]=grafo[clave][valor][0]
+        temp=adicional.dijkstra(grafo_Temp,nodo_Inicial,nodo_FInal)
+    elif n==2:
+        grafo_Temp={}
+        #creo un grafo aparte del cuya en la que la tupla se cambia por el promedio de riesgo y longitud
+        #lo hize así pa cuando toque calcular los 3 caminos distintos 
+        for clave in grafo:
+            grafo_Temp[clave]=grafo[clave]
+            for valor in grafo[clave]:
+                grafo_Temp[clave][valor]=grafo[clave][valor][1]
+        temp=adicional.dijkstra(grafo_Temp,nodo_Inicial,nodo_FInal)
+    elif n==3:
+        grafo_Temp={}
+        #creo un grafo aparte del cuya en la que la tupla se cambia por el promedio de riesgo y longitud
+        #lo hize así pa cuando toque calcular los 3 caminos distintos 
+        for clave in grafo:
+            grafo_Temp[clave]=grafo[clave]
+            for valor in grafo[clave]:
+                grafo_Temp[clave][valor]=grafo[clave][valor][2]
+        temp=adicional.dijkstra(grafo_Temp,nodo_Inicial,nodo_FInal)
     else:
-        grafo[df_Filtrado["origin"][linea]][df_Filtrado["destination"][linea]]=temp
-        
-    if  df_Filtrado["oneway"][linea] == True: 
-      if df_Filtrado["destination"][linea] not in grafo:
-        grafo[df_Filtrado["destination"][linea]]={df_Filtrado["origin"][linea]:temp}
-      else:
-        grafo[df_Filtrado["destination"][linea]][df_Filtrado["origin"][linea]]=temp
-        
-        
-grafo_Temp={}
-
-#creo un grafo aparte del cuya en la que la tupla se cambia por el promedio de riesgo y longitud
-#lo hize así pa cuando toque calcular los 3 caminos distintos 
-for clave in grafo:
-    grafo_Temp[clave]=grafo[clave]
-    for valor in grafo[clave]:
-            grafo_Temp[clave][valor]=grafo[clave][valor][0]
- 
-def dijkstra(inicial,final):
-    camino_mas_Corto={}
-    camino_Recorrido={}
-    nodos_Nousados=grafo_Temp
-    infinito=9999999
-    ruta=[] 
+        print("Por favor digite una opción valida")
     
-    for node in nodos_Nousados:
-        camino_mas_Corto[node]=infinito
-    camino_mas_Corto[inicial]=0
-
-    while nodos_Nousados:
-        distancia_Minima=None
-        for node in nodos_Nousados:
-            if distancia_Minima is None:
-                distancia_Minima=node
-            if camino_mas_Corto[node] < camino_mas_Corto[distancia_Minima]:
-                distancia_Minima=node
-        opciones_Caminos=grafo_Temp[distancia_Minima].items()
+    latitude_list = []
+    longitude_list = [] 
     
-        for tempNodo, indice in opciones_Caminos:
-            if indice + camino_mas_Corto[distancia_Minima] < camino_mas_Corto[tempNodo]:
-                camino_mas_Corto[tempNodo]=indice+camino_mas_Corto[distancia_Minima]
-                camino_Recorrido[tempNodo]=distancia_Minima
-        nodos_Nousados.pop(distancia_Minima)   
-                  
-    nodo_actual=final
-    while nodo_actual != inicial:
-        try:
-            ruta.insert(0,nodo_actual)
-            nodo_actual=camino_Recorrido[nodo_actual]
-        except KeyError:
-            print("La ruta no no es valida")
-            break   
-    ruta.insert(0,inicial)
-    if camino_mas_Corto[final] != infinito:
-        return  ruta
+
+    for i in range (0,len(temp)):
+        valores=str(temp[i])
+        longitude_list.append(float(valores[1:valores.find(",")]))
+        latitude_list.append(float(valores[valores.find(",")+2:len(valores)-1]))
  
-
-
-temp=dijkstra('(-75.5641291, 6.2265514)','(-75.5772295, 6.2507366)')
-
-latitude_list = []
-longitude_list = [] 
+    #esto genera un mapa una pagina en google maps con los puntos   
+    gmap3 = gmplot.GoogleMapPlotter(latitude_list[0],longitude_list[0], 40) 
+    gmap3.scatter( latitude_list, longitude_list, '# FF0000', size = 40, marker = False )   
+    gmap3.plot(latitude_list, longitude_list, 'cornflowerblue', edge_width = 2.5) 
+    gmap3.draw("C:\Programacion_Visual\Datos.py\mapaConPuntos.html")#en este punto se genera el html en la posición que se indique
     
-for i in range (0,len(temp)):
-    valores=str(temp[i])
-    longitude_list.append(float(valores[1:valores.find(",")]))
-    latitude_list.append(float(valores[valores.find(",")+2:len(valores)-1]))
- 
-#esto genera un mapa una pagina en google maps con los puntos
-gmap3 = gmplot.GoogleMapPlotter(latitude_list[0],longitude_list[0], 30) 
-gmap3.scatter( latitude_list, longitude_list, '# FF0000', size = 40, marker = False ) 
-gmap3.plot(latitude_list, longitude_list, 'cornflowerblue', edge_width = 2.5) 
-gmap3.draw( "C:\Programacion_Visual\Datos.py\mapaConPuntos.html" )  #por favor abran el html con el navegador, se genera en la ruta que pongan aqui :)
+    print("Si desea graficar otra ruta escriba si a continuación: ")
+    continuar=str(input())
